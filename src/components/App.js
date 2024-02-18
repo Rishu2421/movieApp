06320e738df4ba497379dec216d81ecd517ae5f6
Rishu Dubey <rishudubay2421@gmail.com>
2024-02-18 12:27:07 +0530

    Learn Dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,34 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar/Navbar.jsx";
 import MovieCard from "./MovieCard/MovieCard";
-function App() {
-  return (
-    <div className="App">
-        <Navbar /> 
-        <div className="main">
-          <div className="tabs">
-            <div className="tab">Movies</div>
-            <div className="tab">Favourites</div>
-          </div>
-          <div className="list">
-            {data.map(movie=>(
-              <MovieCard movie={movie}/>
-            ))}
+class App extends React.Component {
+  componentDidMount(){
+    //make api call
+     this.props.store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+     })
+     console.log("New state",this.props.store.getState())
+  }
+  render(){
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+          <Navbar /> 
+          <div className="main">
+            <div className="tabs">
+              <div className="tab">Movies</div>
+              <div className="tab">Favourites</div>
+            </div>
+            <div className="list">
+              {movies.map((movie,key)=>(
+                <MovieCard key={key} movie={movie}/>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 export default function movies(state=[],action){
-    if(action.types==='ADD_MOVIES'){
+    if(action.type==='ADD_MOVIES'){
         return action.movies;
     }
-    return state
+    return state;
 }
